be8a71d535246b362bd8c000c224775e973d8db4
mkamesh-git <mkamesh319@gmail.com>
2024-04-26 10:25:39 +0000

    code for server health check

---
+++ nodehealth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+####################################
+
+
+#author::: m.kamesh 
+#role:::: devops engineer
+#date::: april/26/2024
+#version::2.34.4
+#
+#
+#
+#
+#
+#
+#####################################
+
+# display nodehealth of server(disc space, free gb, number of cpu)
+
+set -x #debug mode 
+
+df -h     #disc space visual command
+
+free      # gb command of server
+
+
+nproc  # we can see number of cpu
